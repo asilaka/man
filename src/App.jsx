f66e29fb39a05c4e7ded2e4d6aff81339bb257a3
asilaka <abduzalilovasilbek27@gmail.com>
2024-11-03 19:52:53 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Home from './components/Home/Home'
+import Card from './components/Card/Card'
+import Zoom from './components/Zoom/Zoom'
+import Print from './components/Print/Print'
+import Hand from './components/Hand/Hand'
+const App = () => {
+  return (
+  <>
+  <Home/>
+  <Card/>
+  <Zoom/>
+ <Print/>
+ <Hand/>
+   </>
+
+  )
+}
+
+export default App
